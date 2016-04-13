31c5769d3f999facc0b5a93da13939797e4e7bcb
shimizu <shimizu@fenrir.co.jp>
2016-04-13 14:39:57 +0900

    add .gemrc

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .atom .gitconfig .gitignore .pryrc .tmux.conf .vimrc .zshrc )
+DOT_FILES=( .atom .gitconfig .gitignore .gemrc .tmux.conf .vimrc .zshrc .editorconfig )
 
 for file in ${DOT_FILES[@]}
 do
@@ -10,4 +10,5 @@ done
 # install oh-my-zsh
 [ ! -d ~/.oh-my-zsh ] && git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
-[ ! -d ~/.vim/dein ] && mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim && git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim 
+# install dein.vim
+[ ! -d ~/.vim/dein ] && mkdir -p ~/.vim/dein/repos/github.com/Shougo/dein.vim && git clone https://github.com/Shougo/dein.vim.git ~/.vim/dein/repos/github.com/Shougo/dein.vim
