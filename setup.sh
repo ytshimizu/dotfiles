be1d5a78b351f9cc920ac3ecf356113688e749ec
shimizu <l.bird0206@gmail.com>
2016-10-06 18:21:19 +0900

    update .gitconfig

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=( .atom .vim .gitconfig .gitignore .pryrc .gemrc .tmux.conf .vimrc .zshrc .editorconfig )
+DOT_FILES=( .atom .vim .gitconfig .gitignore .pryrc .gemrc .tmux.conf .vimrc .zshrc .editorconfig .gitattributes )
 
 for file in ${DOT_FILES[@]}
 do
